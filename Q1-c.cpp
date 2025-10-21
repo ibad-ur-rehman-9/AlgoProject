e43fac333ea7a44f45b3008009852d922222ce79
Ibad Ur Rehman <ibadamir021@gmail.com>
2025-10-21 17:56:27 +0500

    V2

---
+++ Q1-c.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+Inversions??
+->Those elements in an array where arr[i] > arr[j] but j > i.
+->Basically this is showing how much an array is sorted.
+
+How to count inversions??
+->During each merging step of the merge sort algorithm, we count cross inversions by comparing elements from the left half of the array with those from the right half. If we find an element arr[i] in the left half that is greater than an element arr[j] in the right half, we can conclude that all elements after i in the left half will also be greater than arr[j]. This allows us to count multiple inversions at once. Let's suppose if there are k elements remaining in the left half after i, then there are k cross inversions for that particular arr[j]. The rest of the merging process continues as usual, where we combine the two halves into a sorted array.
+
+*/
+
+/*remember this function only counts the cross inversions i.e inversions when an element of left sub array is bigger than the right sub array. It does not count the inner inversions of both sub arrays.For e.g Consider the array {4, 3, 2, 1}: **Inversions:**
+In left `{4, 3}`: (4,3) → **1 inversion**
+In right `{2, 1}`: (2,1) → **1 inversion**
+Across both: (4,2), (4,1), (3,2), (3,1) → **4 inversions** THIS CROSS INVERSION IS COUNTED BY THIS FUNCTION, NOT THE ABOVE 2.
+*/
+
+int mergeAndCountSplitInversions(vector<int> &arr, int left, int middle, int right)
+{
+
+    int leftSize = middle - left + 1;
+    int rightSize = right - middle;
+
+    vector<int> leftSubarray(leftSize);
+    vector<int> rightSubarray(rightSize);
+
+    for (int i = 0; i < leftSize; i++)
+        leftSubarray[i] = arr[left + i];
+    for (int j = 0; j < rightSize; j++)
+        rightSubarray[j] = arr[middle + 1 + j];
+
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int mergedIndex = left;
+    int inversionCount = 0;
+
+    while (leftIndex < leftSize && rightIndex < rightSize)
+    {
+
+        // no inversion if arr[i] < arr[j]
+        if (leftSubarray[leftIndex] <= rightSubarray[rightIndex])
+        {
+            arr[mergedIndex++] = leftSubarray[leftIndex++];
+        }
+        // If right element is smaller, it forms inversions with all remaining elements in the left subarray
+        else
+        {
+            arr[mergedIndex++] = rightSubarray[rightIndex++];
+            inversionCount += (leftSize - leftIndex);
+        }
+    }
+
+    while (leftIndex < leftSize)
+        arr[mergedIndex++] = leftSubarray[leftIndex++];
+
+    while (rightIndex < rightSize)
+        arr[mergedIndex++] = rightSubarray[rightIndex++];
+
+    return inversionCount;
+}
+
+int countInversionsInRange(vector<int> &arr, int left, int right)
+{
+    int inversionCount = 0;
+
+    if (left < right)
+    {
+        int middle = left + (right - left) / 2;
+
+        inversionCount += countInversionsInRange(arr, left, middle);
+
+        inversionCount += countInversionsInRange(arr, middle + 1, right);
+
+        inversionCount += mergeAndCountSplitInversions(arr, left, middle, right);
+    }
+
+    return inversionCount;
+}
+int inversionCount(vector<int> &arr)
+{
+    return countInversionsInRange(arr, 0, arr.size() - 1);
+}
+int main()
+{
+    vector<int> arr = {1, 3, 5, 10, 2, 6, 8, 9};
+    cout << inversionCount(arr);
+}
---
+++ Q1-e.cpp
@@ -0,0 +1,108 @@
+/*
+One dimensional???
+-> This means we are working on a single number line , therefore we do not have points here in 2D space, in which we have x and y coordinates both.
+
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Creating a class here as we have to return the cloesest distance + the pair of points. So we will return the whole object instead.
+class closestPair
+{
+public:
+    double number1;
+    double number2;
+    double distance;
+
+    closestPair() {};
+    closestPair(double n1, double n2, double d) : number1(n1), number2(n2), distance(d) {}
+};
+
+// This fnction makes every negative distance positive , as distance is always positive.
+double findAbsoluteValue(double x)
+{
+    if (x < 0)
+    {
+        return -x; // E.g if x is -5, it does -(-5) so it becomes 5.
+    }
+    return x;
+}
+
+// Asal function ye hai
+
+closestPair findClosestPair(vector<double> &numberList, int start, int end)
+{
+    int size = end - start;
+
+    if (size <= 1)
+    {
+        return closestPair(0, 0, 1e308); // Base case 1
+    }
+
+    if (size == 2)
+    {
+        double dist = findAbsoluteValue(numberList[start + 1] - numberList[start]);
+        return closestPair(numberList[start], numberList[start + 1], dist); // Base Case 2
+    }
+
+    int middle = start + (end - start) / 2;
+
+    // Recursive Calls for left and right halves
+    closestPair leftResult = findClosestPair(numberList, start, middle);
+    closestPair rightResult = findClosestPair(numberList, middle, end);
+
+    // Find now which half has the smaller distance.
+
+    closestPair currentBest;
+    if (leftResult.distance <= rightResult.distance)
+    {
+        currentBest = leftResult; // Yahan pr deep copy shallow copy wala chakkar nhi hosakta because we don't have any array or a pointer so that's why no need of a copy constructor.
+    }
+    else
+    {
+        currentBest = rightResult;
+    }
+
+    /*Now we have current best. But suppose this was the list {1,9,10,20}
+    Left half: [1, 9] → closest pair is (1, 9) with distance = 8
+    Right half: [10, 20] → closest pair is (10, 20) with distance = 10
+    So current_best.distance = 8
+
+    But wait! What about the pair (9, 10)?
+
+    9 is in the left half
+    10 is in the right half
+    Distance = 1 ← This is actually the closest pair!
+
+    => So now we have to find the cross-half closest pair possibilities.
+
+
+    */
+    for (int i = start; i < middle; i++)
+    { // Every number in left half
+        for (int j = middle; j < end; j++)
+        { // Every number in right half
+            double cross_boundary_distance = findAbsoluteValue(numberList[j] - numberList[i]);
+
+            if (cross_boundary_distance < currentBest.distance)
+            {
+                // Found a better pair that crosses the boundary!
+                currentBest.distance = cross_boundary_distance;
+                currentBest.number1 = numberList[i];
+                currentBest.number2 = numberList[j];
+            }
+        }
+    }
+    return currentBest;
+}
+
+int main()
+{
+    vector<double> list = {2.5, -3, 4, 10, 6};
+    closestPair c = findClosestPair(list, 0, list.size() - 1);
+    cout << "distance: " << c.distance << endl
+         << "Pair: ( " << c.number1 << "," << c.number2 << " )" << endl;
+}
